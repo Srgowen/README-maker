55b214ff9f70a88a08269ebec9663a310c6f9b04
srgowen <srgowen2@gmail.com>
2023-11-09 19:34:38 -0500

    added more options

--- Develop/index.js
+++ Develop/index.js
@@ -1,81 +1,126 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Include packages needed for this application
+// Define a function to generate the table of contents
+function generateTableOfContents(data) {
+    return `
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+`;
+}
+
+// Define a function to generate the badge and license notice
+function generateLicenseSection(license) {
+    return `
+## License
+[![License](https://img.shields.io/badge/License-${license}-blue.svg)](https://opensource.org/licenses/${license})
+
+This application is covered under the ${license} license.
+`;
+}
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-      type: 'input',
-      name: 'title',
-      message: 'Enter the project title:',
+        type: 'input',
+        name: 'title',
+        message: 'Enter the project title:',
     },
     {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a short description explaining the what, why, and how of your project:',
+        type: 'input',
+        name: 'description',
+        message: 'Enter a short description of your project:',
     },
     {
-      type: 'input',
-      name: 'motivation',
-      message: 'What was your motivation?',
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions:',
     },
     {
-      type: 'input',
-      name: 'problem',
-      message: 'What problem does it solve?',
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:',
     },
     {
-      type: 'input',
-      name: 'learned',
-      message: 'What did you learn?',
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'Unlicense'],
     },
     {
-      type: 'input',
-      name: 'credits',
-      message: 'List your collaborators, if any, with links to their GitHub profiles:',
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines:',
     },
     {
-      type: 'input',
-      name: 'thirdPartyAssets',
-      message: 'If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section:',
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions:',
     },
     {
-      type: 'input',
-      name: 'tutorials',
-      message: 'If you followed tutorials, include links to those here as well:',
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
     },
     {
-      type: 'input',
-      name: 'license',
-      message: 'The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/):',
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
     },
-  ];
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(`README.md file has been successfully created.`);
-    }
-  });
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`${fileName} has been successfully created.`);
+        }
+    });
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}\n\n${data.description}\n\n## Motivation\n${data.motivation}\n\n## Problem Solved\n${data.problem}\n\n## What I Learned\n${data.learned}\n\n## Credits\n${data.credits}\n\n## Third-Party Assets\n${data.thirdPartyAssets}\n\n## Tutorials\n${data.tutorials}\n\n## License\n${data.license}\n`;
+    return `# ${data.title}
+
+${generateTableOfContents(data)}
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${generateLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions or need further assistance, feel free to reach out to me via [GitHub](https://github.com/${data.github}) or email at ${data.email}.
+`;
 }
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions).then((userInput) => {
-    const markdown = generateMarkdown(userInput);
-    writeToFile('README.md', markdown);
-  });
+    inquirer.prompt(questions).then((userInput) => {
+        const markdown = generateMarkdown(userInput);
+        writeToFile('README.md', markdown);
+    });
 }
 
 // Function call to initialize app
 init();
-
